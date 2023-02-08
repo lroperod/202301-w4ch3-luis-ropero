d48bdf9a2e8705b67419868462d009dba2b17533
luis <luisrym@gmail.com>
2023-02-08 21:27:35 +0100

    create component Header and his test

---
+++ src/Header/Header.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("Check the header component", () => {
+  test("check de value of my header component", () => {
+    render(<App />);
+    const headerValue = screen.getByText("TodoInput");
+    expect(headerValue).toHaveTextContent("TodoInput");
+  });
+});
